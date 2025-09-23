710d5b69a9dce895f48302af92e346389823e0f1
Harunrkhan <harunrkhan28@gmail.com>
2025-09-23 15:41:10 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,11 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/wner-avatar-Bill-Buddy/',
+  base: '/Bill-Buddy/',
   plugins: [react()],
   server: {
     port: 5173
   }
 })
+
 
